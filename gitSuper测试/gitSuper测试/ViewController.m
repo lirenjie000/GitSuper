e05541c04a07689c0176eb85887eb23f034dbf2f
lirenjie000 <565803433@qq.com>
2016-01-28 18:22:29 +0800

    第一次提交

---
+++ gitSuper测试/gitSuper测试/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  gitSuper测试
+//
+//  Created by lirenjie on 16/1/28.
+//  Copyright © 2016年 李人杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"111");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
